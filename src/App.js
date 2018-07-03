51339d7a8fb634dfa5a0dcae34100427757cec83
jastuccio <jastuccio@mac.com>
2018-07-03 17:55:10 -0400

    Index Files for Organization - Complete Lesson 9

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Toggle from './ToggleRPC';
-import Portal from './Portal';
-import Modal from './Modal';
+import { Toggle, Portal, Modal } from './Utilities';
 
 class App extends Component {
   render() {
@@ -13,7 +11,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-
         <Toggle>
           {
             ({ on, toggle }) => (
