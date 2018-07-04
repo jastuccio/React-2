c5842d304f7ef1dffded600b4d389b46d2ac01bb
jastuccio <jastuccio@mac.com>
2018-07-03 21:28:34 -0400

    React Context API - Complete Lesson 12

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, createContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Toggle } from 'Utilities';
 import { Modal } from 'Elements';
+import User from './User';
+import { UserContext } from './UserContext';
 
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'Test User',
+    email: 'test@user.com'
+  }
+
+  render() {
+    return (
+      <UserContext.Provider
+        value={{
+          user: this.state
+        }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Toggle>
-          {
-            ({ on, toggle }) => (
-              <Fragment>
-                <button onClick={toggle}>Login</button>
-                <Modal on={on} toggle={toggle}>
-                  <h1>Still</h1>
-                </Modal>
-              </Fragment>
-            )
-          }
-        </Toggle>
-      </div>
+      <UserProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <User />
+          <Toggle>
+            {
+              ({ on, toggle }) => (
+                <Fragment>
+                  <button onClick={toggle}>Login</button>
+                  <Modal on={on} toggle={toggle}>
+                    <h1>Still</h1>
+                  </Modal>
+                </Fragment>
+              )
+            }
+          </Toggle>
+        </div>
+      </UserProvider>
     );
   }
 }
